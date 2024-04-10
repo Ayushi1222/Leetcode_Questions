ba3b902ac94a1674c630c179fd4d38511e1192d3
Ayushi katroliya <121269262+Ayushi1222@users.noreply.github.com>
2024-04-10 12:29:25 +0530

    Time: 3 ms (7.18%), Space: 47.9 MB (39.6%) - LeetHub

---
+++ 0275-h-index-ii/0275-h-index-ii.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int hIndex(int[] citations) {
+        int ppr=1;
+        int h=0;
+        for(int i=citations.length-1;i>=0;i--)
+        {
+            h=Math.max(h,Math.min(ppr,citations[i]));
+            ppr++;
+        }
+        return h;
+    }
+}
