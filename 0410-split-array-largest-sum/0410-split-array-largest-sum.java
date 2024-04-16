fd022236fc6dbad524560bf816f5f3269e3fb0d1
Ayushi katroliya <121269262+Ayushi1222@users.noreply.github.com>
2024-04-16 19:05:12 +0530

    Time: 0 ms (100%), Space: 40.9 MB (86.99%) - LeetHub

---
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.java
@@ -0,0 +1,46 @@
+class Solution {
+    public int splitArray(int[] nums, int k) {
+        int start=0;
+        int end=0;
+        int ans=0;
+        for(int i=0;i<nums.length;i++)
+        {
+            end+=nums[i];
+            start=Math.max(start,nums[i]);
+        }
+        while(start<=end)
+        {
+            int mid=(start+end)/2;
+            if(check(nums, k,mid))
+            {
+                ans=mid;
+                end=mid-1;// kyuki ans minimise krna hai har bar
+            }
+            else
+            {
+                start=mid+1;
+            }
+        }
+        return ans;
+    }
+    public static boolean check(int[] nums, int k, int mid)
+    {
+        int s=1;
+        int readpages=0;
+        for(int i=0;i<nums.length;i++)
+        {
+            if(readpages+nums[i]>mid)
+            {
+                s++;
+                readpages=nums[i];
+                if(s>k)
+                return false;
+            }
+            else
+            {
+                readpages+=nums[i];
+            }
+        }
+        return s<=k;
+    }
+}
