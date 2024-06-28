65d046496da975e9f1d9663620fa6310057e599c
Ayushi katroliya <121269262+Ayushi1222@users.noreply.github.com>
2024-06-28 23:17:07 +0530

    Time: 19 ms (48.11%), Space: 61.5 MB (91.35%) - LeetHub

--- 2379-maximum-total-importance-of-roads/2379-maximum-total-importance-of-roads.java
+++ 2379-maximum-total-importance-of-roads/2379-maximum-total-importance-of-roads.java
@@ -1,25 +1,29 @@
+import java.util.*;
+
 class Solution {
     public long maximumImportance(int n, int[][] roads) {
-        int[] freq=new int[n];
-        for (int[] road : roads) {
+        int[] freq = new int[n];
+        for (int[] road : roads) 
+        {
             freq[road[0]]++;
             freq[road[1]]++;
         }
-        Integer[] rank=new Integer[n];
-        for(int i=0;i<n;i++)
+        Integer[] rank = new Integer[n];
+        for (int i = 0; i < n; i++) 
         {
-            rank[i]=i;
+            rank[i] = i;
         }
         Arrays.sort(rank, new Comparator<Integer>() {
             public int compare(Integer a, Integer b) {
                 return Integer.compare(freq[b], freq[a]);
             }
         });
-        long imp=0;
-        for(int i=0;i<n;i++)
+        long t = 0;
+        for (int i = 0; i < n; i++) 
         {
-            imp+=(long)(n-i)*freq[rank[i]];
+            t += (long) (n - i) * freq[rank[i]];
         }
-        return imp;
+        
+        return t;
     }
 }
