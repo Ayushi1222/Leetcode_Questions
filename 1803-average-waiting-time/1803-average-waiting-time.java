a4e7348d53cba11813cf62151b1a135f26c25a3d
Ayushi katroliya <121269262+Ayushi1222@users.noreply.github.com>
2024-07-09 19:41:42 +0530

    Time: 366 ms (5.31%), Space: 84.7 MB (5.31%) - LeetHub

---
+++ 1803-average-waiting-time/1803-average-waiting-time.java
@@ -0,0 +1,15 @@
+class Solution {
+    public double averageWaitingTime(int[][] c) {
+        long curr=c[0][0];
+        long avg=0;
+        long tt=0;
+        for(int i=0;i<c.length;i++)
+        {
+            tt=Math.max(curr,c[i][0])+c[i][1];
+            avg+= tt - c[i][0];
+            curr = tt;
+            System.out.println(avg);
+        }
+        return (double)avg/c.length;
+    }
+}
