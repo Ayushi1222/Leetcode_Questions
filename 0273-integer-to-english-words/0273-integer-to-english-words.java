d6faea415cd10b4974f18389b0bd31e9eb0fadd2
Ayushi katroliya <121269262+Ayushi1222@users.noreply.github.com>
2024-08-08 20:52:41 +0530

    Time: 8 ms (56.9%), Space: 42.5 MB (26.02%) - LeetHub

--- 0273-integer-to-english-words/0273-integer-to-english-words.java
+++ 0273-integer-to-english-words/0273-integer-to-english-words.java
@@ -1,34 +1,32 @@
 class Solution {
-     private static final String[] LESS_THAN_20 = {"", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"};
-    private static final String[] TENS = {"", "Ten", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"};
-    private static final String[] THOUSANDS = {"", "Thousand", "Million", "Billion"};
-
+    private static String[] less_than_10={"","One","Two","Three","Four","Five","Six","Seven","Eight","Nine"};
+    private static String[] less_than_20={"Ten","Eleven","Twelve","Thirteen","Fourteen","Fifteen","Sixteen","Seventeen","Eighteen","Nineteen"};
+    private static String[] less_than_100={"","","Twenty","Thirty","Forty","Fifty","Sixty","Seventy","Eighty","Ninety"};
     public String numberToWords(int num) {
         if(num==0)
         return "Zero";
-        if (num == 0) return "Zero";
-
-        int i = 0;
-        String words = "";
-
-        while (num > 0) {
-            if (num % 1000 != 0)
-                words = helper(num % 1000) + THOUSANDS[i] + " " + words;
-            num /= 1000;
-            i++;
-        }
-
-        return words.trim();
-
+        return solve(num).trim();
     }
-    private static String helper(int num) {
-        if (num == 0)
-            return "";
-        else if (num < 20)
-            return LESS_THAN_20[num] + " ";
-        else if (num < 100)
-            return TENS[num / 10] + " " + helper(num % 10);
-        else
-            return LESS_THAN_20[num / 100] + " Hundred " + helper(num % 100);
+    private String solve(int num)
+    {
+        if(num<10)
+        return less_than_10[num];
+
+        if(num<20)
+        return less_than_20[num%10];
+
+        if(num<100) // 89/10
+        return less_than_100[num/10] + ((num%10 != 0) ? " " + less_than_10[num%10] : "");
+
+        if(num<1000) //549/100
+        return less_than_10[num/100]+ " Hundred" + ((num%100 != 0) ? " " + solve(num%100) : "");
+
+        if(num<1000000) //5496/1000
+        return solve(num/1000)+" Thousand"+((num%1000 != 0) ? " " + solve(num%1000) : "");
+
+        if(num<1000000000) //1234567/1000000
+        return solve(num/1000000)+" Million"+ ((num%1000000 != 0) ? " " + solve(num%1000000) : "");
+
+        return solve(num/1000000000) +" Billion"+ ((num%1000000000 != 0) ? " " + solve(num%1000000000) : "");
     }
 }
