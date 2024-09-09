0359b49a0cfffccd9555858602f8f4ca1f0be4fd
Ayushi katroliya <121269262+Ayushi1222@users.noreply.github.com>
2024-09-10 00:22:49 +0530

    Time: 0 ms (100%), Space: 44.6 MB (85.85%) - LeetHub

--- 1484-linked-list-in-binary-tree/1484-linked-list-in-binary-tree.java
+++ 1484-linked-list-in-binary-tree/1484-linked-list-in-binary-tree.java
@@ -25,24 +25,19 @@
  */
 class Solution {
     public boolean isSubPath(ListNode head, TreeNode root) {
-        return dfs(head, head, root);  // Start DFS search
+        return trackPath(head,head,root);
     }
-
-    // DFS helper function
-    boolean dfs(ListNode head, ListNode cur, TreeNode root) {
-        // If we reach the end of the linked list, return true (successful match)
-        if (cur == null) return true;
-        // If we reach the end of a path in the tree without fully matching the list, return false
-        if (root == null) return false;
-        
-        // Match the current tree node with the current linked list node
-        if (cur.val == root.val) cur = cur.next;
-        // If no match, but the tree node matches the head of the linked list, start a new match
-        else if (head.val == root.val) head = head.next;
-        // Otherwise, reset `cur` to `head` to attempt matching the linked list from scratch
-        else cur = head;
-
-        // Continue DFS down both left and right children
-        return dfs(head, cur, root.left) || dfs(head, cur, root.right);
+    private boolean trackPath(ListNode head, ListNode temp,TreeNode root){
+        if(temp==null) return true;
+        if(root==null) return false;
+        //curr node match kre...
+        if(root.val==temp.val)
+            temp=temp.next;
+        //curr node match na kre but head match kiya tha
+        else if(root.val==head.val)
+        head=head.next;
+        //koi match na kre to reset krdo
+        else temp=head;
+        return trackPath(head,temp,root.left) || trackPath(head,temp,root.right);
     }
 }
