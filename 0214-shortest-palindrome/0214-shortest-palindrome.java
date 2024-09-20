c024700264b934cd6233a01e6884843d1fc5614b
Ayushi katroliya <121269262+Ayushi1222@users.noreply.github.com>
2024-09-20 23:08:44 +0530

    Time: 222 ms (27.16%), Space: 45.3 MB (14.54%) - LeetHub

---
+++ 0214-shortest-palindrome/0214-shortest-palindrome.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String shortestPalindrome(String s) {
+        int n=s.length();
+        String rs=new StringBuilder(s).reverse().toString();
+        for(int i=0;i<n;i++)
+        {
+            if(s.substring(0,n-i).equals(rs.substring(i)))
+            {
+                return new StringBuilder(rs.substring(0,i)).append(s).toString();
+            }
+        }
+        return "";
+    }
+}
