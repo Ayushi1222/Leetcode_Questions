ee2925b0031dcdb773ef0f89f2f70b47fb2106ac
Ayushi katroliya <121269262+Ayushi1222@users.noreply.github.com>
2024-09-27 16:12:01 +0530

    Time: 76 ms (23.5%), Space: 60 MB (69.59%) - LeetHub

--- 3416-sum-of-digit-differences-of-all-pairs/3416-sum-of-digit-differences-of-all-pairs.java
+++ 3416-sum-of-digit-differences-of-all-pairs/3416-sum-of-digit-differences-of-all-pairs.java
@@ -1,14 +1,14 @@
 class Solution {
     public long sumDigitDifferences(int[] nums) {
         long ans=0;
-        int len=String.valueOf(nums[0]).length();
+        int len=(nums[0]+"").length();
         for(int i=0;i<len;i++)
         {
             int[] n=new int[10];
             for(int j=0;j<nums.length;j++)
             {
-                String st=String.valueOf(nums[j]);
-                int digit=Integer.parseInt(String.valueOf(st.charAt(i)));
+                String st=nums[j]+"";
+                int digit=st.charAt(i)-'0';
                 n[digit]++;
                 ans+=(j-n[digit]+1);
             }
