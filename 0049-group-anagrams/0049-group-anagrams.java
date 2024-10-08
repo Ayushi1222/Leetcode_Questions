99a50efbe5cc5112cf43dd877e26227d181b3300
Ayushi katroliya <121269262+Ayushi1222@users.noreply.github.com>
2024-10-08 13:51:20 +0530

    Time: 7 ms (64.83%), Space: 47.3 MB (97.08%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,41 +1,25 @@
 class Solution {
-    HashSet<String> set = new HashSet<>();
-    public List<List<String>> groupAnagrams(String[] str) {
-        List<List<String>> ans = new ArrayList<>();
-        for (int i = 0; i < str.length; i++) 
-        {
-            if (!set.contains(str[i])) 
-            {
-                List<String> l = new ArrayList<>();
-                l.add(str[i]);
-                for (int j = i + 1; j < str.length; j++) 
-                {
-                    boolean flag = check(str[i], str[j]);
-                    if (flag) 
-                    {
-                        set.add(str[j]);
-                        l.add(str[j]);
-                    }
-                }
-                ans.add(l);
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> table = new HashMap<>();
+        for(String str : strs){
+            String key = generateKey(str);
+            if(table.containsKey(key)){
+                table.get(key).add(str);
+            }else{
+                List list = new ArrayList<>();
+                list.add(str);
+                table.put(key, list);
             }
         }
-        return ans;
-    }
-    private boolean check(String s1,String s2)
-    {
-        if (s1.length() != s2.length())
-        return false;
 
-        char[] st1=s1.toCharArray();
-        char[] st2=s2.toCharArray();
-        Arrays.sort(st1);
-        Arrays.sort(st2);
-        for(int i=0;i<st1.length;i++)
-        {
-            if(st1[i]!=st2[i]) 
-            return false;
-        }
-        return true;
+        return new ArrayList<>(table.values());
+        
     }
+
+    private String generateKey(String str){
+        char[] arr = str.toCharArray();
+        Arrays.sort(arr);
+        return new String(arr);
+    }
+        
 }
