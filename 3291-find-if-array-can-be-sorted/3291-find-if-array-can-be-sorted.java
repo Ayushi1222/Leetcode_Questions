f77ca1aba1d14a8061032339462198cff176c9c7
Ayushi katroliya <121269262+Ayushi1222@users.noreply.github.com>
2024-11-07 09:47:25 +0530

    Time: 508 ms (5.56%), Space: 45 MB (5.63%) - LeetHub

---
+++ 3291-find-if-array-can-be-sorted/3291-find-if-array-can-be-sorted.java
@@ -0,0 +1,33 @@
+class Solution {
+    public boolean canSortArray(int[] nums) {
+        for(int i=0;i<nums.length;i++)
+        {
+            for(int j=0;j<nums.length-1;j++)
+            {
+                if(nums[j]>nums[j+1] && sameSetBits(nums[j],nums[j+1]))
+                {
+                    int temp=nums[j];
+                    nums[j]=nums[j+1];
+                    nums[j+1]=temp;
+                }
+                // System.out.println(Arrays.toString(nums));
+            }
+        }
+        for(int i=0;i<nums.length-1;i++)
+        {
+            if(nums[i]>nums[i+1])
+            return false;
+        }
+        return true;
+    }
+    private boolean sameSetBits(int a , int b)
+    {
+        String sta=Integer.toBinaryString(a);
+        String stb=Integer.toBinaryString(b);
+        sta=sta.replaceAll("0","");
+        stb=stb.replaceAll("0","");
+        if(sta.length()==stb.length())
+        return true;
+        return false;
+    }
+}
