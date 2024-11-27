4e6c6848cb4f662bc4e7b168a756aad410b201e5
Ayushi katroliya <121269262+Ayushi1222@users.noreply.github.com>
2024-11-27 19:15:57 +0530

    Time: 4 ms (62.45%), Space: 47.9 MB (9.93%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.java
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -35,19 +35,19 @@ class Solution {
         return dp[dp.length - 1][dp[0].length - 1];
     }
 
-    private int minpath(int[][] grid,int r,int c,int[][] dp)
-    {
-        if (r == grid.length - 1 && c == grid[0].length - 1) {
-			return grid[r][c];
-		}
-        if(r>=grid.length|| c>=grid[0].length)
-        {
-            return Integer.MAX_VALUE;
-        }
-        if(dp[r][c]!=-1)
-        return dp[r][c];
-        int right=minpath(grid,r+1,c,dp);
-        int down=minpath(grid,r,c+1,dp);
-        return dp[r][c]=grid[r][c] + Math.min(right,down);
-    }
+    // private int minpath(int[][] grid,int r,int c,int[][] dp)
+    // {
+    //     if (r == grid.length - 1 && c == grid[0].length - 1) {
+	// 		return grid[r][c];
+	// 	}
+    //     if(r>=grid.length|| c>=grid[0].length)
+    //     {
+    //         return Integer.MAX_VALUE;
+    //     }
+    //     if(dp[r][c]!=-1)
+    //     return dp[r][c];
+    //     int right=minpath(grid,r+1,c,dp);
+    //     int down=minpath(grid,r,c+1,dp);
+    //     return dp[r][c]=grid[r][c] + Math.min(right,down);
+    // }
 }
