d2f2623b91fc76b3179aa272956a7fbe8af7a7f1
Ayushi katroliya <121269262+Ayushi1222@users.noreply.github.com>
2025-01-16 23:51:33 +0530

    Time: 1 ms (100%), Space: 61 MB (87.93%) - LeetHub

--- 2533-bitwise-xor-of-all-pairings/2533-bitwise-xor-of-all-pairings.java
+++ 2533-bitwise-xor-of-all-pairings/2533-bitwise-xor-of-all-pairings.java
@@ -2,25 +2,23 @@ class Solution {
     public int xorAllNums(int[] nums1, int[] nums2) {
         int n1 = nums1.length;
         int n2 = nums2.length;
-        int ans = 0;
-        Map<Integer, Integer> freqMap = new HashMap<>();
-        for (int num : nums1) 
+        int xor1=0;
+        int xor2=0;
+        if(n2%2!=0)
         {
-            freqMap.put(num, freqMap.getOrDefault(num, 0) + n2);
-        }
-        for (int num : nums2) 
-        {
-            freqMap.put(num, freqMap.getOrDefault(num, 0) + n1);
-        }
-        // XOR numbers that appear odd number of times
-        // a^a=0
-        for (int num : freqMap.keySet()) 
-        {
-            if (freqMap.get(num) % 2 == 1) 
+            for (int num : nums1)
             {
-                ans ^= num;
+                xor1^=num;
             }
         }
-        return ans;
+        if(n1%2!=0)
+        {
+            for (int num : nums2)
+            {
+                xor2^=num;
+            }
+        }
+        
+        return xor1^ xor2;
     }
 }
