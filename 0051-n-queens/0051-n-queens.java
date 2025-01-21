fcc47218048a9b0a67b3792b6483decbd1a9feb0
Ayushi katroliya <121269262+Ayushi1222@users.noreply.github.com>
2025-01-21 17:20:37 +0530

    Time: 2 ms (89.36%), Space: 44.7 MB (93.26%) - LeetHub

--- 0051-n-queens/0051-n-queens.java
+++ 0051-n-queens/0051-n-queens.java
@@ -1,62 +1,50 @@
 class Solution {
+
     public List<List<String>> solveNQueens(int n) {
-        boolean [][]check=new boolean[n][n];
-        List<List<String>> l=new ArrayList<>();
-        placed(check,n,0,l);
-        return l;
+        List<List<String>> ans = new ArrayList<>();
+        boolean[] rowCheck = new boolean[n];
+        boolean[] rightDiagonal = new boolean[2 * n - 1];
+        boolean[] leftDiagonal = new boolean[2 * n - 1];
+        List<String> currentSolution = new ArrayList<>();
+
+        solve(n, 0, ans, rowCheck, rightDiagonal, leftDiagonal, currentSolution);
+        return ans;
     }
-      public  void placed(boolean[][] check, int tq, int row,List<List<String>>l) {
-        if (tq == 0) {
-            display(check,l);
-            System.out.println();
+
+    private void solve(int n, int row, List<List<String>> ans,
+                       boolean[] rowCheck, boolean[] rightDiagonal, boolean[] leftDiagonal, 
+                       List<String> currentSolution) {
+        if (row == n) 
+        {
+            ans.add(new ArrayList<>(currentSolution));
             return;
         }
-        for (int col = 0; col < check[0].length; col++) {
-            if (is_it_safe(check, row, col))
-            {
-                check[row][col] = true;
-                placed(check, tq - 1, row + 1,l);
-                check[row][col] = false;
+
+        for (int col = 0; col < n; col++) {
+            int rdIndex = row + col;
+            int ldIndex = n - 1 + col - row;
+
+            if (!rowCheck[col] && !rightDiagonal[rdIndex] && !leftDiagonal[ldIndex]) {
+                // Place the queen and update state
+                rowCheck[col] = true;
+                rightDiagonal[rdIndex] = true;
+                leftDiagonal[ldIndex] = true;
+
+                // Add the current row string to the solution
+                char[] rowString = new char[n];
+                for (int i = 0; i < n; i++) rowString[i] = '.';
+                rowString[col] = 'Q';
+                currentSolution.add(new String(rowString));
+
+                // Recurse to the next row
+                solve(n, row + 1, ans, rowCheck, rightDiagonal, leftDiagonal, currentSolution);
+
+                // Backtrack
+                currentSolution.remove(currentSolution.size() - 1);
+                rowCheck[col] = false;
+                rightDiagonal[rdIndex] = false;
+                leftDiagonal[ldIndex] = false;
             }
         }
-    }
-     public  void display(boolean[][] check,List<List<String>>l)
-      {
-         List<String> l1=new ArrayList<>();
-        for (int i = 0; i < check.length; i++) {
-             String x="";
-            for (int j = 0; j < check[0].length; j++) {
-                if (check[i][j])
-                   x+="Q";
-                else
-                 x+=".";   
-            }
-           l1.add(x);
-        }
-        l.add(l1);
-    }
-      public boolean is_it_safe(boolean[][] check, int row, int col)
-       {
-        for (int i = row; i >= 0; i--) {
-            if (check[i][col])
-                return false;
-        }
-        int j = col;
-        int i=row;
-       while(i>=0 && j<check[0].length)
-       {  if (check[i][j])
-                return false;
-            i--;
-            j++;
-        }
-        j = col;
-       i=row;
-       while(i>=0 && j>=0){
-                if(check[i][j])
-                    return false;
-                i--;
-                j--;
-        }
-            return true;
     }
 }
