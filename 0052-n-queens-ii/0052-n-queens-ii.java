340bbe6f5725a84c03ce704cf2a44a980240d1ce
Ayushi katroliya <121269262+Ayushi1222@users.noreply.github.com>
2025-01-21 17:25:06 +0530

    Time: 0 ms (100%), Space: 40.6 MB (47.65%) - LeetHub

--- 0052-n-queens-ii/0052-n-queens-ii.java
+++ 0052-n-queens-ii/0052-n-queens-ii.java
@@ -1,60 +1,38 @@
 class Solution {
     public int totalNQueens(int n) {
-        boolean [][] board=new boolean[n][n];
-        return queen(board,n,0);
+        boolean[] rowCheck = new boolean[n];
+        boolean[] rightDiagonal = new boolean[2 * n - 1];
+        boolean[] leftDiagonal = new boolean[2 * n - 1];
+        return solve(n, 0, rowCheck, rightDiagonal, leftDiagonal);
     }
 
-    private static int queen(boolean[][] board, int tq,int row)
-    {
-        if(tq==0)
+    private int solve(int n, int row,boolean[] rowCheck, boolean[] rightDiagonal, boolean[] leftDiagonal) {
+        if (row == n) 
         {
+            
             return 1;
         }
         int c=0;
-        for(int col=0;col<board[0].length;col++)
+        for (int col = 0; col < n; col++) 
         {
-            if (isItSafe(board, row, col))
-            {
-                board[row][col] = true;
-                c+=queen(board, tq-1,row+1);
-                board[row][col] = false;
+            int rdIndex = row + col;
+            int ldIndex = n - 1 + col - row;
+
+            if (!rowCheck[col] && !rightDiagonal[rdIndex] && !leftDiagonal[ldIndex]) {
+                // Place the queen and update state
+                rowCheck[col] = true;
+                rightDiagonal[rdIndex] = true;
+                leftDiagonal[ldIndex] = true;
+
+                // Recurse to the next row
+                c+=solve(n, row + 1, rowCheck, rightDiagonal, leftDiagonal);
+
+                // Backtrack
+                rowCheck[col] = false;
+                rightDiagonal[rdIndex] = false;
+                leftDiagonal[ldIndex] = false;
             }
         }
         return c;
-    }
-
-    public static boolean isItSafe(boolean[][] board, int row, int col)
-    {
-        //up col
-        int r=row;
-        while(r>=0)
-        {
-            if(board[r][col]==true)
-                return false;
-            r--;
-        }
-        //left diagonal
-        r=row;
-        int c=col;
-        while(r>=0 && c>=0)
-        {
-            if(board[r][c]==true)
-                return false;
-            r--;
-            c--;
-
-        }
-        //right diagonal
-        r=row;
-        c=col;
-        while(r>=0 && c<board[0].length)
-        {
-            if(board[r][c]==true)
-                return false;
-            r--;
-            c++;
-
-        }
-        return true;
     }
 }
