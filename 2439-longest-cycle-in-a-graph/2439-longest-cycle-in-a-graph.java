89bf8ec549d41b4593bdea593b2dd1bdc362b32c
Ayushi katroliya <121269262+Ayushi1222@users.noreply.github.com>
2025-02-04 10:59:18 +0530

    Time: 29 ms (57.9%), Space: 68.7 MB (53.54%) - LeetHub

--- 2439-longest-cycle-in-a-graph/2439-longest-cycle-in-a-graph.java
+++ 2439-longest-cycle-in-a-graph/2439-longest-cycle-in-a-graph.java
@@ -14,19 +14,25 @@ class Solution {
 
     private void dfs(int i,int[] edges, boolean[] visited,int[] counterArr, int counter)
     {
+        // Count the path saath hi saath
         counter++;
         counterArr[i]=counter;
         visited[i]=true;
         int neighbour=edges[i];
 
+        // Keep moving dfs until cycle detected, if next node is not visited and not equal to -1.
         if(neighbour!=-1 && !visited[neighbour])
         {
             dfs(neighbour, edges,visited,counterArr,counter);
         }
+
+        // If cycle detected, that means the neighbour was also visited in the same cycle
         else if(neighbour!=-1 && counterArr[neighbour]!=0)
         {
             ans=Math.max(ans, counterArr[i]-counterArr[neighbour]+1);
         }
+
+        // Reset the counterArr for backtracking to ensure future mai or longest path mil skte hai then miscalculation na ho.
         counterArr[i]=0;
     }
 }
