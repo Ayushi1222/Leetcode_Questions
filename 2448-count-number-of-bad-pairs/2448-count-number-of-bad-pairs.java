5f00f1b1dab4bc38352f9cefc734324a2707b41e
Ayushi katroliya <121269262+Ayushi1222@users.noreply.github.com>
2025-02-09 15:54:17 +0530

    Time: 34 ms (81.18%), Space: 56.7 MB (90.59%) - LeetHub

--- 2448-count-number-of-bad-pairs/2448-count-number-of-bad-pairs.java
+++ 2448-count-number-of-bad-pairs/2448-count-number-of-bad-pairs.java
@@ -4,8 +4,15 @@ class Solution {
         HashMap<Integer, Integer> map=new HashMap<>();
         for(int i=0;i<nums.length;i++)
         {
+            // j-i=nums[j]-nums[i]
+            // j-nums[j]=i-nums[i]
+            // diff[i]=diff[j]
             int diff=i-nums[i];
+
+            // Good pairs means kya diff aaya hai, if already present hai then get that otherwise 0
             int goodPairs=map.getOrDefault(diff,0);
+
+            // Till i there are i pairs possible with it, ex- for i=3 (0,1,2) are possible.
             badpairs+=i-goodPairs; 
             map.put(diff, goodPairs+1);
         } 
