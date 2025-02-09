74022dc0c603e489b0d583ed6e5d664328202e4f
Ayushi katroliya <121269262+Ayushi1222@users.noreply.github.com>
2025-02-09 17:23:11 +0530

    Time: 1 ms (98.64%), Space: 43.3 MB (78.34%) - LeetHub

--- 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.java
+++ 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.java
@@ -14,63 +14,28 @@
  * }
  */
 class Solution {
+    int maxWidth=0;
     public int widthOfBinaryTree(TreeNode root) {
-        if (root == null) return 0;
-        
-        Queue<Pair<TreeNode, Integer>> q = new ArrayDeque<>();
-        int maxWidth = 0;
-        
-        q.add(new Pair<>(root, 0));
+        // DFS APPROACH
+        if (root == null)
+            return 0;
+        dfs(root, 0, 0, new ArrayList<>());
 
-        while (!q.isEmpty()) {
-            int size = q.size();
-            int minStart = q.peek().getValue(); // Minimum index for normalization
-            
-            int first = 0, last = 0;
-            
-            for (int i = 0; i < size; i++) {
-                Pair<TreeNode, Integer> current = q.poll();
-                TreeNode node = current.getKey();
-                int index = current.getValue();
-                
-                int normalizedIndex = index - minStart; // Normalize the index
-
-                if (i == 0) first = normalizedIndex;
-                if (i == size - 1) last = normalizedIndex;
-
-                if (node.left != null) {
-                    q.offer(new Pair<>(node.left, normalizedIndex * 2 + 1));
-                }
-                if (node.right != null) {
-                    q.offer(new Pair<>(node.right, normalizedIndex * 2 + 2));
-                }
-            }
-            
-            maxWidth = Math.max(maxWidth, last - first + 1);
-        }
-        
         return maxWidth;
     }
-}
 
-class Pair<K,V>{
-    private K node;
-    private V index;
+    private void dfs(TreeNode node, int level, int index, ArrayList<Integer> l) {
+        if (node == null)
+            return;
 
-    public Pair(K node, V index)
-    {
-        this.node=node;
-        this.index=index;
+        if (level == l.size()) {
+            l.add(index);
+        } 
+
+        maxWidth = Math.max(maxWidth, index - l.get(level) + 1);
+
+        dfs(node.left, level + 1, 2 * (index - l.get(level)) + 1, l);
+        dfs(node.right, level + 1, 2 * (index  - l.get(level)) + 2, l);
+
     }
-
-    public K getKey()
-    {
-        return node;
-    }
-
-    public V getValue()
-    {
-        return index;
-    }
-
 }
