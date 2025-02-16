6b9313934c52abcac6f87d715274dd3a0f5c99f9
Ayushi katroliya <121269262+Ayushi1222@users.noreply.github.com>
2025-02-16 08:03:50 +0530

    Time: 147 ms (33.33%), Space: 44.5 MB (44.55%) - LeetHub

--- 2802-find-the-punishment-number-of-an-integer/2802-find-the-punishment-number-of-an-integer.java
+++ 2802-find-the-punishment-number-of-an-integer/2802-find-the-punishment-number-of-an-integer.java
@@ -1,41 +1,34 @@
 class Solution {
-
-    public boolean canPartition(String stringNum, int target) {
-        // Valid Partition Found
-        if (stringNum.isEmpty() && target == 0) {
-            return true;
-        }
-
-        // Invalid Partition Found
-        if (target < 0) {
-            return false;
-        }
-
-        // Recursively check all partitions for a valid partition
-        for (int index = 0; index < stringNum.length(); index++) {
-            String left = stringNum.substring(0, index + 1);
-            String right = stringNum.substring(index + 1);
-            int leftNum = Integer.parseInt(left);
-
-            if (canPartition(right, target - leftNum)) {
-                return true;
-            }
-        }
-        return false;
-    }
-
     public int punishmentNumber(int n) {
         int punishmentNum = 0;
-
-        // Iterate through numbers in range [1, n]
-        for (int currentNum = 1; currentNum <= n; currentNum++) {
-            int squareNum = currentNum * currentNum;
-
-            // Check if valid partition can be found and add squared number if so
-            if (canPartition(Integer.toString(squareNum), currentNum)) {
+        for (int i = 1; i <= n; i++) 
+        {
+            int squareNum = i * i;
+            if (canPartition(Integer.toString(squareNum), i)) 
+            {
                 punishmentNum += squareNum;
             }
         }
         return punishmentNum;
+    }
+    public boolean canPartition(String stringNum, int target) {
+        if (stringNum.isEmpty() && target == 0) {
+            return true;
+        }
+        if (target < 0) {
+            return false;
+        }
+        for (int i = 0; i < stringNum.length(); i++) 
+        {
+            String left = stringNum.substring(0, i + 1);
+            String right = stringNum.substring(i + 1);
+            int leftNum = Integer.parseInt(left);
+
+            if (canPartition(right, target - leftNum)) 
+            {
+                return true;
+            }
+        }
+        return false;
     }
 }
